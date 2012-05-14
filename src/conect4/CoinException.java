c9521f7a9ae117eeefa78c704bd769390ebb44e4
Nihanth Subramanya <nhnt11@gmail.com>
2012-05-14 20:11:22 +0530

    Licensing

--- src/conect4/CoinException.java
+++ src/conect4/CoinException.java
@@ -1,3 +1,21 @@
+/*
+ * CoNect 4  Copyright (C) 2012  Nihanth Subramanya
+ * 
+ * CoNect 4 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * CoNect 4 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with CoNect 4.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
 package conect4;
 
 public class CoinException extends Exception {
